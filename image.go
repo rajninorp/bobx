4adfa0a5a4837792bc6694daf5d2a878ca7af228
boku <boku@tnktmak.com>
2015-07-06 15:09:04 +0900

    画像リンク収集用の関数を追加

---
+++ image.go
@@ -0,0 +1,29 @@
+package bobx
+
+import (
+	"regexp"
+	"strings"
+	"github.com/PuerkitoBio/goquery"
+)
+
+var imageLink []string
+
+func ImageLink(url string) ([]string, error) {
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		var empty []string
+		return empty, err
+	}
+	doc.Find("img").Each(func(_ int, s *goquery.Selection) {
+		link, exist := s.Attr("src")
+		if exist {
+			if m, _ := regexp.MatchString(`/thumbnail.*-preview-.*`, link); m {
+				link = strings.Replace(link, "/thumbnail", "http://www.bobx.com", 1)
+				link = strings.Replace(link, "preview-", "", 1)
+				imageLink = append(imageLink, link)
+			}
+		}
+	})
+	return imageLink, nil
+
+}
